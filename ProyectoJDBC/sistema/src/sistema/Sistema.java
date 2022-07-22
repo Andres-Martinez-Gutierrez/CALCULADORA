979054c6da4273ca185ace09293f2f440b6a2533
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-07-22 09:04:54 -0500

    Se crea Proytecto sistema, con 3 paquetes uno de ellos con una formulario

---
+++ ProyectoJDBC/sistema/src/sistema/Sistema.java
@@ -0,0 +1,15 @@
+/*
+empleadosBL = Paquete para almacenar estructura de informacion que se ira manipulando
+empleadosDAL = paquete para almacenar la interaccion con la base de datos
+empleadosGUI = paquete para almacenar toda la parte de la interfax grafica
+*/
+
+package sistema;
+
+public class Sistema {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
