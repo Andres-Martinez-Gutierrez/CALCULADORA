702f12f17fb4db41a5f8c8aab3a3313371609786
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-07-22 19:53:44 -0500

    Se agregaron los set y get en la clase empleadosBL

--- ProyectoJDBC/sistema/src/sistema/empleadosBL/empleadosBL.java
+++ ProyectoJDBC/sistema/src/sistema/empleadosBL/empleadosBL.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistema.empleadosBL;
 
-/**
- *
- * @author andre
- */
 public class empleadosBL {
+    
+    int Id;
+    String nombre, correo;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
     
 }
