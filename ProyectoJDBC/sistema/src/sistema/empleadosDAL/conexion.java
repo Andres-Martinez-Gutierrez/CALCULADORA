c2c215845e03a27c0fc746a9affc4070bc5ddef1
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-07-22 20:08:36 -0500

    Procesos de verificacion de base de datos

--- ProyectoJDBC/sistema/src/sistema/empleadosDAL/conexion.java
+++ ProyectoJDBC/sistema/src/sistema/empleadosDAL/conexion.java
@@ -2,5 +2,21 @@ package sistema.empleadosDAL;
 import java.sql.*;
 
 public class conexion {
+    String strConexionDB = "";
+    // Variable que me permite conectar la BASE DE DATOS
     
+    Connection conn = null;
+    
+    //try catch
+    public  conexion(){
+        try {
+        Class.forName("org.sqlite.JDBC");
+        conn= DriverManager.getConnection(strConexionDB);
+        System.out.println("Conexion Establecida");
+            
+        } catch (Exception e) {
+            System.out.println("Error de la conexión "+ e);
+        }
+
+    }
 }
